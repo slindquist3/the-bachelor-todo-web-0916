8d3d3e368145c0f2fe98280dfd6c49f6cea3be3e
slindquist3 <slindquist3@gmail.com>
2016-09-20 17:25:51 -0400

    Interated through Bachelor contestants

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,48 @@
 def get_first_name_of_season_winner(data, season)
-  # code here
+   data[season].each do |data_array|
+     firstname = data_array["name"].split(" ")[0]
+     return firstname
+   end
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.each do |season, contestant_array|
+    contestant_array.each do |contestant|
+      if contestant["occupation"].include?(occupation)
+        return contestant["name"]
+      end
+    end
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  count = 0
+  data.each do |season, contestant_array|
+    contestant_array.each do |contestant|
+      if contestant["hometown"].include?(hometown)
+        count += 1
+      end
+    end
+  end
+  return count
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.each do |season, contestant_array|
+    contestant_array.each do |contestant|
+      if contestant["hometown"].include?(hometown)
+        return contestant["occupation"]
+      end
+    end
+  end
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  sum = 0
+  data[season].each do |contestant|
+    sum += contestant["age"].to_f
+  end
+  contestants = data[season].count
+  average = sum / contestants
+  return average.round
 end
